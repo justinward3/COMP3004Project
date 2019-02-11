7e5cf9a301db995b0019566feeb59ce7f0d5d5c9
Justin Ward <justinward@cmail.carleton.ca>
2019-02-10 23:43:06 -0500

    Removed a print stmt. from destructor

--- SmallAnimal.cpp
+++ SmallAnimal.cpp
@@ -5,6 +5,4 @@ using namespace std;
 SmallAnimal::SmallAnimal(QString na, QString col, int a, QChar s, QString t) :
     Animal(na,col,a,s,t){}
 
-SmallAnimal::~SmallAnimal(){cout<<"small animal says goodbye"<<endl;}
-
-
+SmallAnimal::~SmallAnimal(){}
