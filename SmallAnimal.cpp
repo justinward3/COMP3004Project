1473ee1a36cb9f611cb1a38d4d1276629a539afb
Brandon Ward <35843526+BrandonW808@users.noreply.github.com>
2019-03-02 17:08:26 -0500

    Update SmallAnimal.cpp
    Added QDict for Traits

--- SmallAnimal.cpp
+++ SmallAnimal.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <string>
 #include "SmallAnimal.h"
 
-SmallAnimal::SmallAnimal(QString na, QString col, int a, QChar s, QString t) :
-    Animal(na,col,a,s,t){}
+SmallAnimal::SmallAnimal(QString na, QString col, int a, QChar s, QString t, QDict dict) :
+    Animal(na,col,a,s,t,dict){}
 
 SmallAnimal::~SmallAnimal(){}
