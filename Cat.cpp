4ea74a9d91e2eac34f1a234291d313e895d1c745
Brandon Ward <35843526+BrandonW808@users.noreply.github.com>
2019-03-02 17:06:44 -0500

    Update Cat.cpp
    Added QDict for Traits

--- Cat.cpp
+++ Cat.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <string>
 #include "Cat.h"
 
-Cat::Cat(QString na, QString col, int a, QChar s, QString t) :
-            Animal(na,col,a,s,t){}
+Cat::Cat(QString na, QString col, int a, QChar s, QString t, QDict dict) :
+            Animal(na,col,a,s,t,dict){}
 
 Cat::~Cat(){}
