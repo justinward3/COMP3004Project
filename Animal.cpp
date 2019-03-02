7613c46b2abcd3ecccc49b72b321f8672d5d48cc
Brandon Ward <35843526+BrandonW808@users.noreply.github.com>
2019-03-02 17:01:51 -0500

    Update Animal.cpp
    Added traits QDict

--- Animal.cpp
+++ Animal.cpp
@@ -1,11 +1,12 @@
 #include "Animal.h"
 
-Animal::Animal(QString na, QString col, int a, QChar s, QString d) {
+Animal::Animal(QString na, QString col, int a, QChar s, QString d, QDict<int> dict) {
     name = na;
     age = a;
     colour = col;
     sex = s;
     detail = d;
+    traits = dict;
 }
 Animal::~Animal(){}
 
@@ -13,6 +14,7 @@ QString  Animal::getName() const     { return name; }
 int  Animal::getAge() const    { return age; }
 QString  Animal::getColour() const   { return colour; }
 QChar Animal::getSex() const        {return sex; }
+QDict<int> Animal::getTraits() const    {return traits; }
 QString Animal::getDetail() const     {return detail; }
 
 
