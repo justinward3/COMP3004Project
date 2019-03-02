e3a9799cf84354447092c0844b73f76fa7f4d679
Brandon Ward <35843526+BrandonW808@users.noreply.github.com>
2019-03-02 17:05:48 -0500

    Update Bird.cpp
    Added QDict for Traits

--- Bird.cpp
+++ Bird.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <string>
 #include "Bird.h"
 
-Bird::Bird(QString na, QString col, int a, QChar s, QString t) :
-    Animal(na,col,a,s,t){}
+Bird::Bird(QString na, QString col, int a, QChar s, QString t, QDict dict) :
+    Animal(na,col,a,s,t,dict){}
 
 Bird::~Bird(){}
