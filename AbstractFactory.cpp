e347c6a2759f8ceda226a3bbac928fc88c868f5d
Justin Ward <46765652+justinward3@users.noreply.github.com>
2019-03-18 12:14:18 -0400

    first attempt at abstarct factory

---
+++ AbstractFactory.cpp
@@ -0,0 +1,9 @@
+*AnimalFactory createAnimalFactory(){
+ AnimalFactory aF = new AnimalFactory();
+ return aF;
+}
+
+*UserFactory createUserFactory(){
+ UserFactory uF = new UserFactory();
+ return uF;
+}
