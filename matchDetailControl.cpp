2fa4efa32008ed942f754b9d7c6bc72d52eae78f
Sabina <sabina.bialic@gmail.com>
2019-04-08 10:46:40 -0400

    Match details updated

--- matchDetailControl.cpp
+++ matchDetailControl.cpp
@@ -64,7 +64,60 @@ void matchDetailControl::setAnimal(Animal* animal){
 
 void matchDetailControl::setClient(Client* client){
     ui->clientName->setText(client->getFname() + " " + client->getLname());
-    //ui->aAge->setText(QString::number(client->getMatchingPrefs()["age"]));
+
+    // size, space
+    QStringList sml = {"Small", "Medium", "Large"};
+    // experienxe
+    QStringList experience = {"Inexperienced", "Moderately experienced", "Well experienced"};
+    // cost
+    QStringList cost = {"$200 or less", "$200-$500", "$500-$750", "$750-$1000", "$1000 or more"};
+    // time
+    QStringList time = {"Less than 1 hour", "1-2 hours", "2-3 hours", "3+ hours"};
+    // younger, older
+    QStringList yo = {"Older", "Younger"};
+    // lifespan
+    QStringList lifespan = {"1-2", "3-5", "6-10", "10+"};
+    // noise
+    QStringList noise = {"Minimal", "N/A"};
+    // home
+    QStringList home = {"Apartment", "House"};
+    // active
+    QStringList active = {"Not","Somewhate","Moderately","Heavily"};
+    // affection
+    QStringList affection = {"Little", "Moderate", "Lots"};
+    // obedient
+    QStringList obedient = {"Untrained", "Somewhat trained", "Trained"};
+    // shedding
+    QStringList shedding = {"Little", "Moderate", "Lots"};
+    // colour
+    QStringList colour = {"Brown", "Black", "Golden", "Orange", "White", "Grey", "Multi"};
+    // male, female
+    QStringList mf = {"M", "F"};
+    // yes, no
+    QStringList yn = {"Yes", "No"};
+
+    ui->aSize->setText(sml[client->getMatchingPrefs()["size"]-1]);
+    ui->aSpace->setText(sml[client->getMatchingPrefs()["space"]-1]);
+    ui->aExperience->setText(experience[client->getMatchingPrefs()["experience"]-1]);
+    ui->aCost->setText(cost[client->getMatchingPrefs()["cost"]-1]);
+    ui->aTime->setText(time[client->getMatchingPrefs()["time"]-1]);
+    ui->aAge->setText(yo[client->getMatchingPrefs()["age"]-1]);
+    ui->aOlder->setText(yn[client->getMatchingPrefs()["older"]-1]);
+    ui->aLifespan->setText((lifespan[client->getMatchingPrefs()["lifespan"]-1]) + " years");
+    ui->aNoise->setText(noise[client->getMatchingPrefs()["noise"]-1]);
+    ui->aHome->setText(home[client->getMatchingPrefs()["home"]-1]);
+    ui->aActive->setText(active[client->getMatchingPrefs()["active"]-1]);
+    ui->aAffection->setText(affection[client->getMatchingPrefs()["affection"]-1]);
+    ui->aObedient->setText(obedient[client->getMatchingPrefs()["obedient"]-1]);
+    ui->aShedding->setText(shedding[client->getMatchingPrefs()["shedding"]-1]);
+    ui->aColour->setText(colour[client->getMatchingPrefs()["colour"]-1]);
+    ui->aSex->setText(mf[client->getMatchingPrefs()["sex"]-1]);
+    ui->aDogs->setText(yn[client->getMatchingPrefs()["dogs"]-1]);
+    ui->aDogsFuture->setText(yn[client->getMatchingPrefs()["dogsfuture"]-1]);
+    ui->aCats->setText(yn[client->getMatchingPrefs()["cats"]-1]);
+    ui->aCatsFuture->setText(yn[client->getMatchingPrefs()["catsfuture"]-1]);
+    ui->aChildren->setText(yn[client->getMatchingPrefs()["children"]-1]);
+    ui->aChildrenFuture->setText(yn[client->getMatchingPrefs()["childrenfuture"]-1]);
 }
 
 //set mw pointer
