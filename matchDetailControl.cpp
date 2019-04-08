12a7195ff76c469d26fe7c970629060f8cd97f24
Sabina <sabina.bialic@gmail.com>
2019-04-08 09:51:36 -0400

    Added to match detail view

--- matchDetailControl.cpp
+++ matchDetailControl.cpp
@@ -21,18 +21,33 @@ void matchDetailControl::setAnimal(Animal* animal){
     ui->animalName->setText(animal->getName());
     ui->animalSex->setText(animal->getSex());
     ui->animalAge->setText(QString::number(animal->getAge()));
+    ui->animalLifespan->setText(QString::number(animal->getTraits()["lifespan"]));
     ui->animalColour->setText(animal->getColour());
     ui->animalDetail->setText(animal->getDetail());
-    QStringList activeList= {"None","Little","Moderate","Lots"};
-    QStringList spaceList= {"Little", "Moderate", "Lots"};
-    ui->animalActiveness->setText(activeList[animal->getTraits()["activeness"]-1]);
-    ui->animalSpace->setText(spaceList[animal->getTraits()["space"]-1]);
     ui->animalCost->setText(QString::number((animal->getTraits())["cost"]));
-    ui->animalTime->setText(QString::number((animal->getTraits())["time"]));
+    ui->animalTime->setText((QString::number((animal->getTraits())["time"])) + " hrs.");
+
+    // difficulty
+    QStringList docList = {"Easy", "Intermediate", "Difficult"};
+    // loudness, activeness, shedding
+    QStringList nlmlList= {"None","Little","Moderate","Lots"};
+    // space, affection
+    QStringList lmlList= {"Little", "Moderate", "Lots"};
+    // obedience
+    QStringList obedience = {"Untrained", "Somewhat trained", "Trained"};
+
+    ui->animalDifficulty->setText(docList[animal->getTraits()["DoC"]-1]);
+    ui->animalLoudness->setText(nlmlList[animal->getTraits()["loudness"]-1]);
+    ui->animalActiveness->setText(nlmlList[animal->getTraits()["activeness"]-1]);
+    ui->animalShedding->setText(nlmlList[animal->getTraits()["shedding"]-1]);
+    ui->animalSpace->setText((lmlList[animal->getTraits()["space"]-1]) + " space");
+    ui->animalAffection->setText(lmlList[animal->getTraits()["affection"]-1]);
+    ui->animalObedience->setText(docList[animal->getTraits()["obedience"]-1]);
 }
 
 void matchDetailControl::setClient(Client* client){
     ui->clientName->setText(client->getFname() + " " + client->getLname());
+    //ui->aAge->setText(QString::number(client->getMatchingPrefs()["age"]));
 }
 
 //set mw pointer
